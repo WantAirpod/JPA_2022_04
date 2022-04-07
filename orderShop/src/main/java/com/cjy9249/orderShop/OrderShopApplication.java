5203131125cf78fc1429922fe150d9d926277c8b
JINYOUNGCHOI <cjy9249@tmon.co.kr>
2022-04-07 12:18:11 +0900

    feat : project clone

---
+++ orderShop/src/main/java/com/cjy9249/orderShop/OrderShopApplication.java
@@ -0,0 +1,13 @@
+package com.cjy9249.orderShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderShopApplication.class, args);
+	}
+
+}
---
+++ orderShop/src/test/java/com/cjy9249/orderShop/OrderShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjy9249.orderShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
