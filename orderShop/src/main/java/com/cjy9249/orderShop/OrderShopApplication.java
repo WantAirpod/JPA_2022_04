6bd2f317cacce2626d035dec60bdd8fff765e49d
JINYOUNGCHOI <cjy9249@tmon.co.kr>
2022-04-07 23:06:51 +0900

    feat : entity 설계 및 gradle, yml 설정

--- orderShop/src/main/java/com/cjy9249/orderShop/OrderShopApplication.java
+++ orderShop/src/main/java/com/cjy9249/orderShop/OrderShopApplication.java
@@ -3,6 +3,9 @@ package com.cjy9249.orderShop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * created by cjy 2022.04.
+ */
 @SpringBootApplication
 public class OrderShopApplication {
 
