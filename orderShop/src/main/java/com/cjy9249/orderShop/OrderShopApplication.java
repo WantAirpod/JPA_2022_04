e2327dd3ad2e0a1d323debd763ffa89c07718ab9
JINYOUNGCHOI <cjy9249@tmon.co.kr>
2022-04-08 00:27:12 +0900

    feat : config 설정

--- orderShop/src/main/java/com/cjy9249/orderShop/OrderShopApplication.java
+++ orderShop/src/main/java/com/cjy9249/orderShop/OrderShopApplication.java
@@ -3,13 +3,16 @@ package com.cjy9249.orderShop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 /**
  * created by cjy 2022.04.
  */
 @SpringBootApplication
 public class OrderShopApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)throws IOException
+	{
 		SpringApplication.run(OrderShopApplication.class, args);
 	}
 
